46a15d70764c9ec31ba58ca7c92a72a36c03c51b
Basele Stephen <s.basele@gmail.com>
2020-05-12 21:38:31 +0300

    Installed NPM Dependencies

--- public/js/app.js
+++ public/js/app.js
@@ -49940,8 +49940,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\Team153Product1\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\Team153Product1\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/bakulmi/laravelT/Team-153-Product/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/bakulmi/laravelT/Team-153-Product/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
